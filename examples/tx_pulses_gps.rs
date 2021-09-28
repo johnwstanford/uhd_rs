f84da6976eaf884a5d5be89a448545c360484446
John Stanford <johnwstanford@gmail.com>
2021-09-27 21:27:02 -0500

    Reading all sensor values in tx_pulses_gps

--- examples/tx_pulses_gps.rs
+++ examples/tx_pulses_gps.rs
@@ -2,6 +2,7 @@
 use std::collections::HashSet;
 
 use uhd_rs::usrp::USRP;
+use uhd_rs::types::sensors::DataType;
 
 fn main() -> Result<(), &'static str> {
     
@@ -20,6 +21,16 @@ fn main() -> Result<(), &'static str> {
     println!("Clock source: {:?}", usrp.get_clock_source(0)?);
     println!("Sensor names: {:#?}", sensor_names);
 
+    for sensor in sensor_names.iter() {
+        let sensor_value = usrp.get_mboard_sensor(sensor, 0)?;
+        let sensor_type = sensor_value.get_data_type()?;
+        match sensor_type {
+            DataType::Boolean => println!("{}: {}", sensor, sensor_value.to_bool()?),
+            DataType::Integer => println!("{}: {}", sensor, sensor_value.to_int()?),
+            DataType::RealNum => println!("{}: {}", sensor, sensor_value.to_realnum()?),
+            DataType::String  => println!("{}: {}", sensor, sensor_value.get_value()?),
+        }
+    }
 
     Ok(())
 }
