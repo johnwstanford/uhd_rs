5b6ad6097ad7bd6eb3f4676cd453283f0ea0beee
John Stanford <johnwstanford@gmail.com>
2023-10-17 19:14:03 -0500

    Include all dwells in the phase comparison plot

--- scripts/twinrx_phase_compare.py
+++ scripts/twinrx_phase_compare.py
@@ -16,7 +16,7 @@ import matplotlib.pyplot as plt
 FNAME_RE = regex.compile('([^/]+)_([AB])([01])_([^M]+)MHz_(\\d+)dB_([^M]+)Msps')
 MAX_SAMPLES = 200000
 
-channels = dict()
+dwells = dict()
 
 for fname in os.listdir('..'):
     m = FNAME_RE.match(fname)
@@ -24,8 +24,8 @@ for fname in os.listdir('..'):
         continue
 
     impl = m.group(1)
-    if 'twinrx' not in impl:
-        continue
+    if impl not in dwells:
+        dwells[impl] = dict()
     
     slot = m.group(2)
     chan = int(m.group(3))
@@ -33,7 +33,7 @@ for fname in os.listdir('..'):
     gain_db = m.group(5)
     fs_msps = float(m.group(6))
 
-    f = open('../%s'%fname, 'rb')
+    f = open('../' + fname, 'rb')
     buff = f.read(4)
 
     waveform = []
@@ -51,23 +51,28 @@ for fname in os.listdir('..'):
     N = len(waveform)
     dc_ofs = (re_sum/N) + 1j*(im_sum/N)
         
-    channels['%s%i'%(slot, chan)] = [x-dc_ofs for x in waveform]
+    dwells[impl]['%s%i'%(slot, chan)] = [x-dc_ofs for x in waveform]
 
 for k0, k1, sp in [('A0', 'A1', 321), ('A0', 'B0', 322), ('A1', 'B0', 323), 
                    ('A0', 'B1', 324), ('A1', 'B1', 325), ('B0', 'B1', 326)]:
     
-    wf0 = channels[k0]
-    wf1 = channels[k1]
-    N = min(len(wf0), len(wf1))
-    
-    phase_cmp = [wf0[i] * np.conj(wf1[i]) for i in range(N)]
-    
     plt.subplot(sp)
-    plt.title('%s vs %s'%(k0, k1))
-    plt.hist([np.angle(x) for x in phase_cmp], bins=75, ec='k')
+
+    for impl in dwells.keys():
         
+        dwell = dwells[impl]
+        wf0 = dwell[k0]
+        wf1 = dwell[k1]
+        N = min(len(wf0), len(wf1))
         
+        phase_cmp = [wf0[i] * np.conj(wf1[i]) for i in range(N)]
         
+        plt.title('%s vs %s'%(k0, k1))
+        plt.hist([np.angle(x) for x in phase_cmp], bins=75, ec='k', label=impl)
+
+    
+    
+    
         
         
         
