86bf7119d72b7bba86b7137d0733d0f384f3b1a7
John Stanford <johnwstanford@gmail.com>
2025-02-13 12:23:38 -0600

    Update 000_get_device_info.rs

--- examples/000_get_device_info.rs
+++ examples/000_get_device_info.rs
@@ -52,6 +52,20 @@ fn main() -> Result<(), &'static str> {
 		println!("  Antenna: {}", tx_info.antenna()?);
 
 		println!("  Num channels: {}", usrp.tx_num_channels()?);
+		
+		let sensor_names:HashSet<String> = usrp.get_mboard_sensor_names(0)?.into_iter().collect();
+		println!("Sensor names: {:#?}", sensor_names);
+		
+		for sensor in sensor_names.iter() {
+            let sensor_value = usrp.get_mboard_sensor(sensor, 0)?;
+            let sensor_type = sensor_value.get_data_type()?;
+            match sensor_type {
+                DataType::Boolean => println!("    {}: {}", sensor, sensor_value.to_bool()?),
+                DataType::Integer => println!("    {}: {}", sensor, sensor_value.to_int()?),
+                DataType::RealNum => println!("    {}: {}", sensor, sensor_value.to_realnum()?),
+                DataType::String  => println!("    {}: {}", sensor, sensor_value.get_value()?),
+            }
+        }
 	}
 
  	Ok(())
